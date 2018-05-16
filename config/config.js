e585630ee35655d5c67ea04559db0cc7556cbedf
Dom Harrington <dom@harrington-mail.com>
2018-05-16 10:46:39 -0700

    Only load config from __dirname

--- config/config.js
+++ config/config.js
@@ -2,4 +2,4 @@
 // Have to disable this rule because we're using a fake
 // `config` module as documented here:
 // https://github.com/alexindigo/configly#migration-from-config
-module.exports = require('configly')();
+module.exports = require('configly')(__dirname);
