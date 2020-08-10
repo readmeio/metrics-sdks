64e12744e5b52cc7d5ab0c485795b433daac379d
Anthony M <anthony.moffa@thoughtbot.com>
2020-08-10 15:11:39 -0400

    Setup Ruby github action
    This commit sets up a basic Ruby github action that runs the specs for
    the gem.

--- packages/ruby/spec/readme/metrics_spec.rb
+++ packages/ruby/spec/readme/metrics_spec.rb
@@ -2,8 +2,4 @@ RSpec.describe Readme::Metrics do
   it "has a version number" do
     expect(Readme::Metrics::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
