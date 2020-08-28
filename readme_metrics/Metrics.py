6b1869920c684928596746461438a67a57947c8a
Jon Ursenbach <erunion@users.noreply.github.com>
2020-08-28 11:28:28 -0700

    ci: python workflow (#53)
    * chore: capitalizing the python readme filename
    
    * ci: setting up a python ci flow
    
    * ci: setting up dependabot on the python sdk
    
    * style: instituting PEP8 with pycodestyle
    
    * style: running pycodestyle on the tests dir
    
    * test: moving unit tests over to using pytest
    
    * ci: running pytest in ci
    
    * style: using python black in lieu of pycodestyle
    
    * style: running black on all the python code
    
    * style: reverting a change to how the default GROUPING_FUNCTION is
    
    * style: re-running black

--- readme_metrics/Metrics.py
+++ readme_metrics/Metrics.py
@@ -12,39 +12,39 @@ from readme_metrics.PayloadBuilder import PayloadBuilder
 
 class Metrics:
     """
-    This is the internal central controller classinvoked by the WSGI
-    middleware. It handles the creation, queueing,
-    and submission of the requests.
+    This is the internal central controller classinvoked by the WSGI middleware. It
+    handles the creation, queueing, and submission of the requests.
     """
-    PACKAGE_NAME: str = 'readme/metrics'
-    METRICS_API: str = 'https://metrics.readme.io'
 
     def __init__(self, config: MetricsApiConfig):
         """
-        Constructs and initializes the ReadMe Metrics controller class with
-        the specified configuration.
-        :param config: Running configuration
+        Constructs and initializes the ReadMe Metrics controller class with the
+        specified configuration.
+
+        Args:
+            config (MetricsApiConfig): Running configuration
         """
 
         self.config = config
         self.payload_builder = PayloadBuilder(
-                                config.BLACKLIST,
-                                config.WHITELIST,
-                                config.IS_DEVELOPMENT_MODE,
-                                config.GROUPING_FUNCTION)
+            config.BLACKLIST,
+            config.WHITELIST,
+            config.IS_DEVELOPMENT_MODE,
+            config.GROUPING_FUNCTION,
+        )
         self.queue = queue.Queue()
 
     def process(self, request: Request, response: ResponseInfoWrapper) -> None:
-        """
-        Enqueues a request/response combination to be submitted
-        to the ReadMe Metrics API.
-        :param request: werkzeug.Request request object
-        :param response: ResponseInfoWrapper response object
+        """Enqueues a request/response combination to be submitted the API.
+
+        Args:
+            request (Request): Request object
+            response (ResponseInfoWrapper): Response object
         """
         self.queue.put(self.payload_builder(request, response))
 
-        if(self.queue.qsize() >= self.config.BUFFER_LENGTH):
-            if(self.config.IS_BACKGROUND_MODE):
+        if self.queue.qsize() >= self.config.BUFFER_LENGTH:
+            if self.config.IS_BACKGROUND_MODE:
                 threading.Thread(target=self._processAll, daemon=True).start()
             else:
                 self._processAll()
@@ -58,16 +58,14 @@ class Metrics:
 
         payload = json.dumps(result_list)
 
-        # print("Posting: " + payload)
-
         version = pkg_resources.require("readme_metrics")[0].version
 
-        readme_result = requests.post(self.METRICS_API + "/request",
-                                      auth=(self.config.README_API_KEY, ""),
-                                      data=payload,
-                                      headers={
-                                        'Content-Type': 'application/json',
-                                        'User-Agent': 'readme-metrics-python@' + version
-                                      })
-
-        # print("Response: " + readme_result.text)
+        readme_result = requests.post(
+            self.METRICS_API + "/request",
+            auth=(self.config.README_API_KEY, ""),
+            data=payload,
+            headers={
+                "Content-Type": "application/json",
+                "User-Agent": "readme-metrics-python@" + version,
+            },
+        )
--- readme_metrics/PayloadBuilder.py
+++ readme_metrics/PayloadBuilder.py
@@ -14,77 +14,83 @@ from readme_metrics.util import util_exclude_keys, util_filter_keys
 
 class PayloadBuilder:
     """
-    Internal builder class that handles the construction of the request and
-    response portions of the payload sent to the ReadMe API.
+    Internal builder class that handles the construction of the request and response
+    portions of the payload sent to the ReadMe API.
 
-    ...
-    Attributes
-    ----------
-    blacklist: List[str]
-        Cached blacklist for current PayloadBuilder instance
-    whitelist: List[str]
-        Cached whitelist for current PayloadBuilder instance
-    development_mode: bool
-        Cached development mode parameter for current PayloadBuilder instance
-    grouping_function: function
-        Cached grouping function for current PayloadBuilder instance
+    Attributes:
+        blacklist (List[str]): Cached blacklist for current PayloadBuilder instance
+        whitelist (List[str]): Cached whitelist for current PayloadBuilder instance
+        development_mode (bool): Cached development mode parameter for current
+            PayloadBuilder instance
+        grouping_function ([type]): Cached grouping function for current PayloadBuilder
+            instance
     """
 
-    def __init__(self, blacklist: List[str], whitelist: List[str], development_mode: bool, grouping_function):
-        """
-        Creates a PayloadBuilder instance with the supplied configuration
+    def __init__(
+        self,
+        blacklist: List[str],
+        whitelist: List[str],
+        development_mode: bool,
+        grouping_function,
+    ):
+        """Creates a PayloadBuilder instance with the supplied configuration
 
-        :param blacklist: Header/JSON body blacklist
-        :param whitelist: Header/JSON body whitelist
-        :param development_mode: Development mode flag passed to ReadMe
-        :param grouping_function: Grouping function to generate an identity payload
+        Args:
+            blacklist (List[str]): Header/JSON body blacklist
+            whitelist (List[str]): Header/JSON body whitelist
+            development_mode (bool): Development mode flag passed to ReadMe
+            grouping_function ([type]): Grouping function to generate an identity
+                payload
         """
-
         self.blacklist = blacklist
         self.whitelist = whitelist
-        self.development_mode = 'true' if development_mode else 'false'
+        self.development_mode = "true" if development_mode else "false"
         self.grouping_function = grouping_function
 
     def __call__(self, request: Request, response: ResponseInfoWrapper) -> dict:
-        """
-        Builds a HAR payload encompassing the request & response data, to be
-        sent to the ReadMe API
+        """Builds a HAR payload encompassing the request & response data
 
-        :param request: Request information to use
-        :param response: Response information to use
-        :return: Payload object (ready to be serialized and sent to ReadMe)
+        Args:
+            request (Request): Request information to use
+            response (ResponseInfoWrapper): Response information to use
+
+        Returns:
+            dict: Payload object (ready to be serialized and sent to ReadMe)
         """
         payload = {
-            'group': self.grouping_function(request),
-            'clientIPAddress': request.remote_addr,
-            'development': self.development_mode,
-            'request': {
-                'log': {
-                    'creator': {
-                        'name': __name__,
-                        'version': pkg_resources.require("readme_metrics")[0].version,
-                        'comment': sys.version
+            "group": self.grouping_function(request),
+            "clientIPAddress": request.remote_addr,
+            "development": self.development_mode,
+            "request": {
+                "log": {
+                    "creator": {
+                        "name": __name__,
+                        "version": pkg_resources.require("readme_metrics")[0].version,
+                        "comment": sys.version,
                     },
-                    'entries': [{
-                        'pageref': request.base_url,
-                        'startedDateTime': request.rm_start_dt,
-                        'time': (int(time.time() * 1000) - request.rm_start_ts),
-                        'request': self._build_request_payload(request),
-                        'response': self._build_response_payload(response)
-                    }]
+                    "entries": [
+                        {
+                            "pageref": request.base_url,
+                            "startedDateTime": request.rm_start_dt,
+                            "time": int(time.time() * 1000) - request.rm_start_ts,
+                            "request": self._build_request_payload(request),
+                            "response": self._build_response_payload(response),
+                        }
+                    ],
                 }
-            }
-
+            },
         }
 
         return payload
 
     def _build_request_payload(self, request: Request) -> dict:
-        """
-        Wraps the request portion of the payload
+        """Wraps the request portion of the payload
 
-        :param request: Request object containing the response information
-        :return: Wrapped request payload
+        Args:
+            request (Request): Request object containing the response information
+
+        Returns:
+            dict: Wrapped request payload
         """
         post_data = {}
         headers = None
@@ -98,7 +104,7 @@ class PayloadBuilder:
 
         if request.content_length is not None and request.content_length > 0:
 
-            body = request.rm_body.decode('utf-8') or ''
+            body = request.rm_body.decode("utf-8") or ""
 
             try:
                 json_object = json.loads(body)
@@ -108,45 +114,43 @@ class PayloadBuilder:
                 elif self.whitelist:
                     body = util_filter_keys(json_object, self.whitelist)
 
-                post_data['mimeType'] = 'application/json'
-                post_data['text'] = body
+                post_data["mimeType"] = "application/json"
+                post_data["text"] = body
             except ValueError as e:
-                post_data['params'] = [body]
+                post_data["params"] = [body]
 
                 if request.content_type:
-                    post_data['mimeType'] = request.content_type
+                    post_data["mimeType"] = request.content_type
                 else:
-                    post_data['mimeType'] = 'text/html'
+                    post_data["mimeType"] = "text/html"
 
         hdr_items = []
-
         for k, v in headers.items():
-            hdr_items.append({'name': k, 'value': v})
+            hdr_items.append({"name": k, "value": v})
 
         qs_items = []
         qs_dict = dict(parse.parse_qsl(request.query_string.decode("utf-8")))
 
         for k, v in qs_dict.items():
-            qs_items.append({'name': k, 'value': v})
-
-        # hdr_items = headers.items()
-        # har_header_array = [{'name' : k, 'value' : hdr_items[k]} for k in hdr_items]
+            qs_items.append({"name": k, "value": v})
 
         return {
-            'method': request.method,
-            'url': request.base_url,
-            'httpVersion': request.environ['SERVER_PROTOCOL'],
-            'headers': hdr_items, #list(headers.items()),
-            'queryString': qs_items, #request.query_string.decode("utf-8"),
-            **post_data
+            "method": request.method,
+            "url": request.base_url,
+            "httpVersion": request.environ["SERVER_PROTOCOL"],
+            "headers": hdr_items,
+            "queryString": qs_items,
+            **post_data,
         }
 
     def _build_response_payload(self, response: ResponseInfoWrapper) -> dict:
-        """
-        Wraps the response portion of the payload
+        """Wraps the response portion of the payload
 
-        :param response: ResponseInfoWrapper containing the response information
-        :return: Wrapped response payload
+        Args:
+            response (ResponseInfoWrapper): containing the response information
+
+        Returns:
+            dict: Wrapped response payload
         """
         if self.blacklist:
             headers = util_exclude_keys(response.headers, self.blacklist)
@@ -167,24 +171,20 @@ class PayloadBuilder:
         except ValueError:
             pass
 
-        # hdr_items = headers.items()
-        # har_header_array = [{'name' : k, 'value' : hdr_items[k]} for k in hdr_items]
-
         hdr_items = []
-
         for k, v in headers.items():
-            hdr_items.append({'name': k, 'value': v})
+            hdr_items.append({"name": k, "value": v})
 
-        status_code = int(response.status.split(' ')[0])
-        status_text = response.status.replace(str(status_code) + ' ', '')
+        status_code = int(response.status.split(" ")[0])
+        status_text = response.status.replace(str(status_code) + " ", "")
 
         return {
-            'status': status_code,
-            'statusText': status_text or '',
-            'headers': hdr_items, #headers.items(),
-            'content': {
-                'text': body,
-                'size': response.content_length,
-                'mimeType': response.content_type
-            }
+            "status": status_code,
+            "statusText": status_text or "",
+            "headers": hdr_items,  # headers.items(),
+            "content": {
+                "text": body,
+                "size": response.content_length,
+                "mimeType": response.content_type,
+            },
         }
