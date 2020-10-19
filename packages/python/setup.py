b4f3587b9e50e8024c2bb38bd0853cf879266abb
Jon Ursenbach <jon@ursenba.ch>
2020-10-19 11:48:33 -0700

    build: bumping python to 1.0.4

--- packages/python/setup.py
+++ packages/python/setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="readme-metrics",
-    version="1.0.3",
+    version="1.0.4",
     author="ReadMe",
     author_email="support@readme.io",
     description="ReadMe API Metrics WSGI SDK",
