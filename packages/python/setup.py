c1ffbebac28c82fdc0dabb17b43b8c2bb21593e6
Dmitry Pimenov <dmitry@readme.io>
2020-12-07 12:32:35 -0800

    chore(version): bumping python metrics sdk version

--- packages/python/setup.py
+++ packages/python/setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="readme-metrics",
-    version="1.0.4",
+    version="1.0.5",
     author="ReadMe",
     author_email="support@readme.io",
     description="ReadMe API Metrics WSGI SDK",
