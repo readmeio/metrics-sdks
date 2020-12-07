e967a944880fdaaff8ed0f0b3ae36730444b966a
Dmitry Pimenov <dmitryspimenov@gmail.com>
2020-12-07 12:44:18 -0800

    Update setup.py

--- packages/python/setup.py
+++ packages/python/setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="readme-metrics",
-    version="1.0.5",
+    version="1.0.6",
     author="ReadMe",
     author_email="support@readme.io",
     description="ReadMe API Metrics WSGI SDK",
