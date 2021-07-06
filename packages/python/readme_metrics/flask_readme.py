bb2c8c703221d3d8ef2a72d5ad8d0ac5d88bc9e7
Ryan Park <ryanp@readme.io>
2021-07-06 16:04:54 -0700

    fix(flask): resolve issues with request/response bodies

--- packages/python/readme_metrics/flask_readme.py
+++ packages/python/readme_metrics/flask_readme.py
@@ -20,9 +20,7 @@ class ReadMeMetrics:
             self.init_app(app)
 
     def init_app(self, app: Flask):
-        self.config.LOGGER.info(
-            f"Configuring {app.name} hooks to call ReadMeMetrics extension functions"
-        )
+        self.config.LOGGER.info(f"Configuring {app.name} hooks to call ReadMeMetrics extension functions")
         app.before_request(self.before_request)
         app.after_request(self.after_request)
 
@@ -30,9 +28,9 @@ class ReadMeMetrics:
         try:
             request.rm_start_dt = str(datetime.now())
             request.rm_start_ts = int(time.time() * 1000)
-            # TODO when applicable (on POST?), set
-            # req.rm_content_length = content_length
-            # req.rm_body = content_body
+            if "Content-Length" in request.headers or request.data:
+                request.rm_content_length = request.headers["Content-Length"] or "0"
+                request.rm_body = request.data or ""
         except Exception as e:
             # Errors in the Metrics SDK should never cause the application to
             # throw an error. Log it but don't re-raise.
@@ -45,7 +43,7 @@ class ReadMeMetrics:
                 response.status,
                 content_type=None,
                 content_length=None,
-                body=response.data.decode("utf-8"),  # TODO what about non-utf8?
+                body=response.data,
             )
             self.metrics_core.process(request, response_info)
         except Exception as e:
