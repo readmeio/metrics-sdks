a133332974b77d129fe534d42bf5b24b0b8a34b9
Dom Harrington <domharrington@protonmail.com>
2022-04-19 14:50:56 +0100

    Revert "chore: move dotnet repo into packages/dotnet (#391)"
    This reverts commit d6ee495dfa9907b36d6c1e72c200bfd463a9c1c4.

--- bin/split.sh
+++ bin/split.sh
@@ -20,10 +20,8 @@ remote sdks-node git@github.com:readmeio/metrics-sdks-node.git
 remote sdks-php git@github.com:readmeio/metrics-sdks-php.git
 remote sdks-python git@github.com:readmeio/metrics-sdks-python.git
 remote sdks-ruby git@github.com:readmeio/metrics-sdks-ruby.git
-remote sdks-dotnet git@github.com:readmeio/metrics-sdks-dotnet.git
 
 split 'packages/node' sdks-node
 split 'packages/php' sdks-php
 split 'packages/python' sdks-python
 split 'packages/ruby' sdks-ruby
-split 'packages/dotnet' sdks-dotnet
