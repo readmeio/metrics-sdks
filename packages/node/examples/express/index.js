ed914cdc54e1b8b0b3d8ca4da838f67159fc8495
Dom Harrington <domharrington@protonmail.com>
2022-04-21 16:44:44 +0100

    docs: add basic metrics integration to express app

--- packages/node/examples/express/index.js
+++ packages/node/examples/express/index.js
@@ -1,13 +1,29 @@
 import express from 'express';
+import readmeio from 'readmeio';
+
+if (!process.env.README_API_KEY) {
+  // eslint-disable-next-line no-console
+  console.error('Missing `README_API_KEY` environment variable');
+  process.exit(1);
+}
 
 const app = express();
+const port = process.env.PORT || 4000;
+
+app.use(
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  readmeio.expressMiddleware(process.env.README_API_KEY, req => ({
+    apiKey: Math.random().toString(36).substring(2),
+    label: 'Owlbert',
+    email: 'owlbert@example.com',
+  }))
+);
 
 app.get('/', (req, res) => {
   res.json({ message: 'hello world' });
 });
 
-const server = app.listen(process.env.PORT, 'localhost', function () {
-  const { address, port } = server.address();
+const server = app.listen(port, 'localhost', function () {
   // eslint-disable-next-line no-console
-  console.log('Example app listening at http://%s:%s', address, port);
+  console.log('Example app listening at http://%s:%s', server.address().address, port);
 });
