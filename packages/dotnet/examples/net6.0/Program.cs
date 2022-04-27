78ffd0079975016524897ea36f562ddc1d89d755
Dom Harrington <domharrington@protonmail.com>
2022-04-27 18:25:26 +0100

    test(integration/dotnet): add very basic .net 6.0 app for metrics
    Still outstanding:
    - make the sdk respect `METRICS_SERVER` env variable
    - try and get `readme__apiKey` better as an env variable
    - wire it up to an integration test

--- packages/dotnet/examples/net6.0/Program.cs
+++ packages/dotnet/examples/net6.0/Program.cs
@@ -1,6 +1,26 @@
+if (string.IsNullOrEmpty(Environment.GetEnvironmentVariable("README_API_KEY"))) {
+  Console.Error.WriteLine("Missing `README_API_KEY` environment variable");
+  System.Environment.Exit(1);
+}
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+var port = Environment.GetEnvironmentVariable("PORT");
+if (string.IsNullOrEmpty(port)) port = "4000";
 
-app.Run();
+app.Use(async (context, next) => {
+  context.Items["apiKey"] = "owlbert-api-key";
+  context.Items["label"] = "Owlbert";
+  context.Items["email"] = "owlbert@example.com";
+
+  await next();
+});
+
+app.UseMiddleware<Readme.Metrics>();
+
+app.MapGet("/", async context => {
+  await context.Response.WriteAsJsonAsync(new { message = "hello world" });
+});
+
+app.Run($"http://localhost:{port}");
---
+++ packages/dotnet/examples/net6.0/README.md
@@ -0,0 +1,18 @@
+# ReadMe Metrics .NET 6.0 Demo
+
+## Install
+
+```sh
+brew install dotnet
+```
+
+## Run
+<!-- TODO make it so that it takes README_API_KEY -->
+```sh
+readme__apiKey=<Your ReadMe API Key here> dotnet run
+```
+
+## Test
+```sh
+curl http://localhost:4000
+```
