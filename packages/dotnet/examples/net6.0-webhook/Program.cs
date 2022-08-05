ccc6cb745d75d23d32ea386a96269b26fd6394ce
Dom Harrington <domharrington@protonmail.com>
2022-08-05 17:58:42 +0100

    feat: more progress on .net webhooks code sample, not yet working

--- packages/dotnet/examples/net6.0-webhook/Program.cs
+++ packages/dotnet/examples/net6.0-webhook/Program.cs
@@ -1,9 +1,12 @@
-using System.Text.RegularExpressions;
+using System.Text;
+using static System.Text.Encoding;
+using System.Security.Cryptography;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 var port = Environment.GetEnvironmentVariable("PORT") ?? "4000";
+var secret = Environment.GetEnvironmentVariable("README_API_KEY") ?? "";
 
 app.MapPost("/webhook", async context =>
 {
@@ -22,15 +25,31 @@ app.MapPost("/webhook", async context =>
     })
     .ToLookup(item => item.Key, item => item.Value);
 
-    var time = DateTimeOffset.FromUnixTimeMilliseconds(long.Parse(input["t"].First())).DateTime;
-    if (DateTime.Now.Subtract(time).Minutes > 30)
+    var time = input["t"].First();
+    if (DateTime.Now.Subtract(DateTimeOffset.FromUnixTimeMilliseconds(long.Parse(time)).DateTime).Minutes > 30)
     {
       throw new Exception("Expired Signature");
     }
 
     var readmeSignature = input["v0"].First();
+    var json = await new StreamReader(context.Request.Body).ReadToEndAsync();
+    var unsigned = time + "." + json;
+    var hmac = HMAC.Create("HMACSHA256");
+    hmac.Initialize();
+    hmac.Key = Unicode.GetBytes(secret);
+    var result = Unicode.GetBytes(unsigned);
+    var hash = hmac.ComputeHash(result);
 
-    // TODO check for validity of signature
+    var verifySignature = new StringBuilder(hash.Length * 2);
+    foreach (byte b in hash)
+    {
+      verifySignature.AppendFormat("{0:x2}", b);
+    }
+
+    if (verifySignature.ToString() != readmeSignature)
+    {
+      throw new Exception("Invalid Signature");
+    }
   }
   catch (Exception)
   {
