73a0fcb5f465e2b747f825634834a9218308cceb
Dom Harrington <domharrington@protonmail.com>
2022-08-08 16:42:09 +0100

    chore(python): lint

--- packages/python/examples/flask/webhook.py
+++ packages/python/examples/flask/webhook.py
@@ -12,42 +12,59 @@ if os.getenv("README_API_KEY") is None:
 
 app = Flask(__name__)
 
+
 @app.post("/webhook")
 def webhook():
-  try:
-    signature = request.headers.get('readme-signature', None)
+    try:
+        signature = request.headers.get("readme-signature", None)
 
-    if signature is None:
-      raise Exception("Missing Signature")
+        if signature is None:
+            raise Exception("Missing Signature")
 
-    input = dict((x.strip(), y.strip())
-             for x, y in (element.split('=')
-             for element in signature.split(',')))
+        input = dict(
+            (x.strip(), y.strip())
+            for x, y in (element.split("=") for element in signature.split(","))
+        )
 
-    time = input["t"];
+        time = input["t"]
 
-    if datetime.now() - datetime.fromtimestamp(int(input["t"])/1000) > timedelta(minutes=30):
-      raise Exception("Expired Signature")
+        if datetime.now() - datetime.fromtimestamp(int(input["t"]) / 1000) > timedelta(
+            minutes=30
+        ):
+            raise Exception("Expired Signature")
 
-    body = request.get_json();
-    unsigned = time + "." + json.dumps(body, separators=(',', ':'))
-    verify_signature = hmac.new(os.getenv("README_API_KEY").encode('utf8'), unsigned.encode('utf8'), 'sha256').hexdigest()
-    readme_signature = input['v0'];
-    if hmac.compare_digest(verify_signature.encode(), readme_signature.encode('utf8')) is False:
-      raise Exception("Invalid Signature")
+        body = request.get_json()
+        unsigned = time + "." + json.dumps(body, separators=(",", ":"))
+        verify_signature = hmac.new(
+            os.getenv("README_API_KEY").encode("utf8"),
+            unsigned.encode("utf8"),
+            "sha256",
+        ).hexdigest()
+        readme_signature = input["v0"]
+        if (
+            hmac.compare_digest(
+                verify_signature.encode(), readme_signature.encode("utf8")
+            )
+            is False
+        ):
+            raise Exception("Invalid Signature")
 
-  except Exception as e:
+    except Exception as e:
+        return (
+            {"error": str(e)},
+            401,
+            {"Content-Type": "application/json; charset=utf-8"},
+        )
+
     return (
-        {"error": str(e)},
-        401,
+        {
+            "petstore_auth": "default-key",
+            "basic_auth": {"user": "user", "pass": "pass"},
+        },
+        200,
         {"Content-Type": "application/json; charset=utf-8"},
     )
 
-  return (
-      {"petstore_auth": "default-key", "basic_auth": { "user": "user", "pass": "pass"}},
-      200,
-      {"Content-Type": "application/json; charset=utf-8"},
-  )
 
 if __name__ == "__main__":
     app.run(debug=False, host="127.0.0.1", port=os.getenv("PORT", 4000))
