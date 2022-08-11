6336c76ef88ac69f3a51c8b6d866eb7ec332d7ff
Dom Harrington <domharrington@protonmail.com>
2022-08-11 09:29:41 +0100

    feat(dotnet): v2.0.0

--- packages/dotnet/ReadMe/ConstValues.cs
+++ packages/dotnet/ReadMe/ConstValues.cs
@@ -5,7 +5,7 @@ namespace ReadMe
   public static class ConstValues
   {
     public static readonly string name = "readmeio.net";
-    public static readonly string version = "1.1.2";
+    public static readonly string version = "2.0.0";
     public static readonly int wait = 0;
     public static readonly string receive = null;
     public static string readmeApiEndpoints
