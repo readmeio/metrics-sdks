256e1b3fadcca9ad62852cc065c7f9e1546bd7a7
Jon Ursenbach <jon@ursenba.ch>
2022-08-16 15:45:44 -0700

    chore: pairing down the laravel example

--- packages/php/examples/laravel/app/Providers/RouteServiceProvider.php
+++ packages/php/examples/laravel/app/Providers/RouteServiceProvider.php
@@ -2,23 +2,11 @@
 
 namespace App\Providers;
 
-use Illuminate\Cache\RateLimiting\Limit;
 use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\Facades\Route;
 
 class RouteServiceProvider extends ServiceProvider
 {
-    /**
-     * The path to the "home" route for your application.
-     *
-     * Typically, users are redirected here after authentication.
-     *
-     * @var string
-     */
-    public const HOME = '/home';
-
     /**
      * Define your route model bindings, pattern filters, and other route configuration.
      *
@@ -45,8 +33,6 @@ class RouteServiceProvider extends ServiceProvider
      */
     protected function configureRateLimiting()
     {
-        RateLimiter::for('api', function (Request $request) {
-            return Limit::perMinute(60)->by($request->user()?->id ?: $request->ip());
-        });
+        //
     }
 }
--- packages/php/examples/laravel/routes/api.php
+++ packages/php/examples/laravel/routes/api.php
@@ -14,6 +14,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::get('/user', function () {
+    return [
+        'api_key' => session()->getId(),
+        'email' => 'buster@example.com',
+    ];
 });
