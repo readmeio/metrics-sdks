c6a3e7cacfae808d6aacdd3498d312bf57a7fade
Jon Ursenbach <jon@ursenba.ch>
2022-08-22 12:39:25 -0700

    refactor: cleaning up how we're adding variables into snippets

--- packages/sdk-snippets/src/helpers/code-builder.ts
+++ packages/sdk-snippets/src/helpers/code-builder.ts
@@ -10,17 +10,31 @@ export class CodeBuilder extends HTTPSnippetCodeBuilder {
     server?: Record<string, { line: number }>;
   } = {};
 
-  variable = (type: 'security' | 'server', name: string) => {
-    if (!this.variableRanges[type]) {
-      this.variableRanges[type] = {};
-    }
-
-    this.variableRanges[type][name] = {
-      line: this.code.length,
-    };
-  };
-
   ranges = () => {
     return this.variableRanges;
+  };
+
+  /**
+   * Push a dynamic piece of code to the snippet and record where in the snippet it was added.
+   *
+   */
+  pushVariable = (
+    line: string,
+    opts: {
+      name: string;
+      indentationLevel?: number;
+      type: 'security' | 'server';
+    }
+  ) => {
+    this.push(line, opts.indentationLevel);
+
+    // Record where in the snippet this variable is located.
+    if (!this.variableRanges[opts.type]) {
+      this.variableRanges[opts.type] = {};
+    }
+
+    this.variableRanges[opts.type][opts.name] = {
+      line: this.code.length,
+    };
   };
 }
--- packages/sdk-snippets/src/targets/dotnet/net6/webhooks/client.ts
+++ packages/sdk-snippets/src/targets/dotnet/net6/webhooks/client.ts
@@ -16,7 +16,7 @@ export const net6: Client = {
       ...options,
     };
 
-    const { blank, join, push, ranges, variable } = new CodeBuilder({ indent: opts.indent });
+    const { blank, join, push, pushVariable, ranges } = new CodeBuilder({ indent: opts.indent });
 
     push('var builder = WebApplication.CreateBuilder(args);');
     push('var app = builder.Build();');
@@ -60,13 +60,16 @@ export const net6: Client = {
     if (server.length) {
       push('// OAS Server variables', 2);
       server.forEach(data => {
-        push(
+        pushVariable(
           `${escapeForObjectKey(data.name, true)} = "${escapeForDoubleQuotes(
             data.default || data.default === '' ? data.default : data.name
           )}",`,
-          2
+          {
+            type: 'server',
+            name: data.name,
+            indentationLevel: 2,
+          }
         );
-        variable('server', data.name);
       });
     }
 
@@ -80,19 +83,25 @@ export const net6: Client = {
         if (data.type === 'http') {
           // Only HTTP Basic auth has any special handling for supplying auth.
           if (data.scheme === 'basic') {
-            push(`${escapeForObjectKey(data.name, true)} = new { user = "user", pass = "pass" },`, 2);
-            variable('security', data.name);
+            pushVariable(`${escapeForObjectKey(data.name, true)} = new { user = "user", pass = "pass" },`, {
+              type: 'security',
+              name: data.name,
+              indentationLevel: 2,
+            });
             return;
           }
         }
 
-        push(
+        pushVariable(
           `${escapeForObjectKey(data.name, true)} = "${escapeForDoubleQuotes(
             data.default || data.default === '' ? data.default : data.name
           )}",`,
-          2
+          {
+            type: 'security',
+            name: data.name,
+            indentationLevel: 2,
+          }
         );
-        variable('security', data.name);
       });
     }
 
