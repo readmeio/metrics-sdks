0768b7fb51bd5a7eab2ff0adb15d9a96b1f9903d
Dom Harrington <domharrington@protonmail.com>
2022-08-29 17:27:17 +0100

    feat(ruby): add very basic integration test scaffolding

---
+++ packages/ruby/examples/metrics-rails/app/controllers/metrics_controller.rb
@@ -0,0 +1,16 @@
+class MetricsController < ApplicationController
+  def index
+    render json: {"message" => "hello world"}
+  end
+
+  def post
+    head :ok
+  end
+
+  def webhook
+    render json: {
+      "petstore_auth": "default-key",
+      "basic_auth": { "user": "user", "pass": "pass" },
+    }
+  end
+end
