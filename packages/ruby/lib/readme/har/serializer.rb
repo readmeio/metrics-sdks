668eb264110c76c4ca1fed3087de92edeee2a14a
Dom Harrington <domharrington@protonmail.com>
2022-09-01 15:49:05 +0100

    fix(ruby): update to use UTC formatted date for startedDateTime

--- packages/ruby/lib/readme/har/serializer.rb
+++ packages/ruby/lib/readme/har/serializer.rb
@@ -46,7 +46,7 @@ module Readme
             timings: timings,
             request: request,
             response: response,
-            startedDateTime: @start_time.iso8601,
+            startedDateTime: @start_time.utc.iso8601(3),
             time: elapsed_time
           }
         ]
