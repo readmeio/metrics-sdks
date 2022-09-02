e49b1b9ffa8ea3fb980c0b6a00384173a0164ed6
Dom Harrington <domharrington@protonmail.com>
2022-09-02 16:24:45 +0100

    chore(integration/ruby/rails): switch app to use Readme::Webhook.verify

--- packages/ruby/examples/metrics-rails/app/controllers/metrics_controller.rb
+++ packages/ruby/examples/metrics-rails/app/controllers/metrics_controller.rb
@@ -1,20 +1,4 @@
-def verify_webhook(body, signature, secret)
-  raise 'Missing Signature' unless signature
-
-  parsed = signature.split(',').each_with_object({ time: -1, readme_signature: '' }) do |item, accum|
-    k, v = item.split('=')
-    accum[:time] = v if k.eql? 't'
-    accum[:readme_signature] = v if k.eql? 'v0'
-  end
-
-  # Make sure timestamp is recent to prevent replay attacks
-  raise 'Expired Signature' if Time.now.utc - Time.at(0, parsed[:time].to_i, :millisecond).utc > 30.minutes
-
-  # Verify the signature is valid
-  unsigned = "#{parsed[:time]}.#{body}"
-  mac = OpenSSL::HMAC.hexdigest('SHA256', secret, unsigned)
-  raise 'Invalid Signature' if mac != parsed[:readme_signature]
-end
+require 'readme/webhook'
 
 class MetricsController < ApplicationController
   def index
@@ -29,7 +13,7 @@ class MetricsController < ApplicationController
     signature = request.headers['readme-signature']
 
     begin
-      verify_webhook(request.raw_post, signature, ENV.fetch('README_API_KEY', nil))
+      Readme::Webhook.verify(request.raw_post, signature, ENV.fetch('README_API_KEY', nil))
     rescue RuntimeError => e
       render json: { error: e.message }, status: 401
       return
