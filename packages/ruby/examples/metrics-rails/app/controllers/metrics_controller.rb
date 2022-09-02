9461b1ad0a01cc08f314f459db34ce274932f549
Dom Harrington <domharrington@protonmail.com>
2022-09-02 15:30:24 +0100

    chore(integration/ruby/rails): lint

--- packages/ruby/examples/metrics-rails/app/controllers/metrics_controller.rb
+++ packages/ruby/examples/metrics-rails/app/controllers/metrics_controller.rb
@@ -1,28 +1,19 @@
 def verify_webhook(body, signature, secret)
   raise 'Missing Signature' unless signature
 
-  parsed = signature.split(',').reduce({ time: -1, readme_signature: '' }){|accum,item|
+  parsed = signature.split(',').each_with_object({ time: -1, readme_signature: '' }) do |item, accum|
     k, v = item.split('=')
-    if k.eql? 't'
-      accum[:time] = v
-    end
-    if k.eql? 'v0'
-      accum[:readme_signature] = v
-    end;
-    accum
-  }
+    accum[:time] = v if k.eql? 't'
+    accum[:readme_signature] = v if k.eql? 'v0'
+  end
 
   # Make sure timestamp is recent to prevent replay attacks
-  if Time.now.utc - Time.at(0, parsed[:time].to_i, :millisecond).utc > 30.minutes
-    raise 'Expired Signature'
-  end
+  raise 'Expired Signature' if Time.now.utc - Time.at(0, parsed[:time].to_i, :millisecond).utc > 30.minutes
 
   # Verify the signature is valid
-  unsigned = parsed[:time] + '.' + body
-  mac = OpenSSL::HMAC.hexdigest("SHA256", secret, unsigned)
-  if mac != parsed[:readme_signature]
-    raise 'Invalid Signature'
-  end
+  unsigned = "#{parsed[:time]}.#{body}"
+  mac = OpenSSL::HMAC.hexdigest('SHA256', secret, unsigned)
+  raise 'Invalid Signature' if mac != parsed[:readme_signature]
 end
 
 class MetricsController < ApplicationController
@@ -39,9 +30,8 @@ class MetricsController < ApplicationController
 
     begin
       verify_webhook(request.raw_post, signature, ENV.fetch('README_API_KEY', nil))
-    rescue RuntimeError => e 
-      puts e.message
-      render :json => { error: e.message }, status: 401
+    rescue RuntimeError => e
+      render json: { error: e.message }, status: 401
       return
     end
 
