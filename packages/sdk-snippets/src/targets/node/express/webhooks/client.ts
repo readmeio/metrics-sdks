97e170ff7931098f7b83f246ceda09139c5ec86f
Kanad Gupta <kgupta@umn.edu>
2022-09-26 15:22:54 -0500

    chore: copy update

--- packages/sdk-snippets/src/targets/node/express/webhooks/client.ts
+++ packages/sdk-snippets/src/targets/node/express/webhooks/client.ts
@@ -112,7 +112,10 @@ export const express: Client = {
 
     push('const port = 8000;');
     push("const server = app.listen(port, '0.0.0.0', function () {");
-    push("console.log('Webhooks example app listening at http://%s:%s', server.address().address, port);", 1);
+    push(
+      "console.log('Personalized Docs Webhook example app listening at http://%s:%s', server.address().address, port);",
+      1
+    );
     push('});');
     blank();
 
