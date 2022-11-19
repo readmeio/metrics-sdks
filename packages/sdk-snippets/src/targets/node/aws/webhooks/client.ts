013c0bfa751270eeb0d8f4bdde5951202f85e42f
Ryan Park <ryanp@readme.io>
2022-11-18 16:04:20 -0800

    feat(nodejs): add support for OAS variables + toggle for creating new keys

--- packages/sdk-snippets/src/targets/node/aws/webhooks/client.ts
+++ packages/sdk-snippets/src/targets/node/aws/webhooks/client.ts
@@ -1,3 +1,5 @@
+// For use with AWS Lambda Runtime: nodejs16.x
+
 import type { Client } from '../../../targets';
 
 import { CodeBuilder } from '../../../../helpers/code-builder';
@@ -13,7 +15,11 @@ export const aws: Client = {
   convert: ({ secret, security, server }, options) => {
     const opts = {
       indent: '  ',
-      readOnly: true,
+      createKeys: false,
+      // We don't currently provide a value for defaultUsagePlanId in the
+      // ReadMe app, since we don't have any knowledge of our customers' usage
+      // plans -- but we might choose to later, and it's helpful for testing.
+      defaultUsagePlanId: '123abc',
       ...options,
     };
 
@@ -21,121 +27,31 @@ export const aws: Client = {
       indent: opts.indent,
     });
 
-    // push("const express = require('express');");
-    // push("const readme = require('readmeio');");
-
-    // blank();
-
-    // push('const app = express();');
-    // blank();
-
-    // push('// Your ReadMe secret');
-    // push(`const secret = '${secret}';`);
-    // blank();
-
-    // push("app.post('/webhook', express.json({ type: 'application/json' }), async (req, res) => {");
-    // startSection('verification');
-    // push('// Verify the request is legitimate and came from ReadMe.', 1);
-    // push("const signature = req.headers['readme-signature'];", 1);
-    // blank();
-
-    // push('try {', 1);
-    // push('readme.verifyWebhook(req.body, signature, secret);', 2);
-    // push('} catch (e) {', 1);
-    // push('// Handle invalid requests', 2);
-    // push('return res.status(401).json({ error: e.message });', 2);
-    // push('}', 1);
-    // endSection('verification');
-    // blank();
-
-    // startSection('payload');
-    // push('// Fetch the user from the database and return their data for use with OpenAPI variables.', 1);
-    // push('// const user = await db.find({ email: req.body.email })', 1);
-    // push('return res.json({', 1);
-
-    // if (!server.length && !security.length) {
-    //   push('// Add custom data to return in your webhook call here.', 2);
-    // }
-
-    // if (server.length) {
-    //   push('// OAS Server variables', 2);
-    //   server.forEach(data => {
-    //     pushVariable(
-    //       `${escapeForObjectKey(data.name)}: '${escapeForSingleQuotes(
-    //         data.default || data.default === '' ? data.default : data.name
-    //       )}',`,
-    //       {
-    //         type: 'server',
-    //         name: data.name,
-    //         indentationLevel: 2,
-    //       }
-    //     );
-    //   });
-    // }
-
-    // if (server.length && security.length) {
-    //   blank();
-    // }
-
-    // if (security.length) {
-    //   push('// OAS Security variables', 2);
-    //   security.forEach(data => {
-    //     if (data.type === 'http') {
-    //       // Only HTTP Basic auth has any special handling for supplying auth.
-    //       if (data.scheme === 'basic') {
-    //         pushVariable(`${escapeForObjectKey(data.name)}: { user: 'user', pass: 'pass' },`, {
-    //           type: 'security',
-    //           name: data.name,
-    //           indentationLevel: 2,
-    //         });
-    //         return;
-    //       }
-    //     }
-
-    //     pushVariable(
-    //       `${escapeForObjectKey(data.name)}: '${escapeForSingleQuotes(
-    //         data.default || data.default === '' ? data.default : data.name
-    //       )}',`,
-    //       {
-    //         type: 'security',
-    //         name: data.name,
-    //         indentationLevel: 2,
-    //       }
-    //     );
-    //   });
-    // }
-
-    // push('});', 1);
-    // endSection('payload');
-    // push('});');
-
-    // blank();
-
-    // push("const server = app.listen(8000, '0.0.0.0', () => {");
-    // push("console.log('Example app listening at http://%s:%s', server.address().address, server.address().port);", 1);
-    // push('});');
-    // blank();
-
-    // (Internal ReadMe note) This is an example Personalized Docs webhook handler
-    // for customers using AWS API Gateway. In this example we attempt to lookup the
-    // API key for the user with the given email address by using the AWS SDK method
-    // GetApiKeys. It returns a JSON response with { "apiKey": "whatever" }, or a
-    // 404 if the user doesn't exist.
-
-    // For use with this AWS Lambda Runtime: nodejs16.x
-    // Create API keys: no
-
-    push("const { APIGatewayClient, GetApiKeysCommand } = require('@aws-sdk/client-api-gateway');");
+    if (opts.createKeys) {
+      push('const {');
+      push('APIGatewayClient,', 1);
+      push('CreateApiKeyCommand,', 1);
+      push('CreateUsagePlanKeyCommand,', 1);
+      push('GetApiKeysCommand,', 1);
+      push("} = require('@aws-sdk/client-api-gateway');");
+    } else {
+      push("const { APIGatewayClient, GetApiKeysCommand } = require('@aws-sdk/client-api-gateway');");
+    }
     push("const readme = require('readmeio');");
     blank();
 
     push('// Your ReadMe secret; you may want to store this in AWS Secrets Manager');
-    // @todo: This should be populated with the project's JWT secret
     push(`const README_SECRET = '${secret}';`);
     blank();
 
+    if (opts.createKeys) {
+      push('// Your default API Gateway usage plan; this will be attached to the API keys that being created');
+      push(`const DEFAULT_USAGE_PLAN_ID = '${opts.defaultUsagePlanId}';`);
+      blank();
+    }
+
     push('exports.handler = async event => {');
-    push('let statusCode, apiKey, error;', 1);
+    push('let statusCode, email, apiKey, error;', 1);
     blank();
 
     push('try {', 1);
@@ -147,18 +63,41 @@ export const aws: Client = {
     blank();
 
     startSection('payload');
+    const getCommandName = opts.createKeys ? 'getCommand' : 'command';
     push('const email = body.email;', 2);
     push('const client = new APIGatewayClient();', 2);
-    push('const command = new GetApiKeysCommand({ nameQuery: email, includeValues: true });', 2);
-    push('const keys = await client.send(command);', 2);
+    push(`const ${getCommandName} = new GetApiKeysCommand({ nameQuery: email, includeValues: true });`, 2);
+    push(`const keys = await client.send(${getCommandName});`, 2);
     push('if (keys.items.length > 0) {', 2);
-    // if multiple API keys are returned for the given email, use the first one
-    // @todo Talk to Gabe about when/whether to return "keys" as an array of { apiKey, id } or { user, id } objects
+    push('// if multiple API keys are returned for the given email, use the first one', 3);
     push('apiKey = keys.items[0].value;', 3);
     push('statusCode = 200;', 3);
     push('} else {', 2);
-    push("error = 'Email not found';", 3);
-    push('statusCode = 404;', 3);
+    if (opts.createKeys) {
+      push('const createKeyCommand = new CreateApiKeyCommand({', 3);
+      push('name: email,', 4);
+      push('description: `API key for ReadMe user ${email}`,', 4);
+      push('tags: {', 4);
+      push('user: email,', 5);
+      push("vendor: 'ReadMe',", 5);
+      push('},', 4);
+      push('enabled: true,', 4);
+      push('});', 3);
+      push('const key = await client.send(createKeyCommand);', 3);
+      blank();
+      push('const usagePlanKeyCommand = new CreateUsagePlanKeyCommand({', 3);
+      push('usagePlanId: DEFAULT_USAGE_PLAN_ID,', 4);
+      push('keyId: key.id,', 4);
+      push("keyType: 'API_KEY',", 4);
+      push('});', 3);
+      push('await client.send(usagePlanKeyCommand);', 3);
+      blank();
+      push('apiKey = key.value;', 3);
+      push('statusCode = 200;', 3);
+    } else {
+      push("error = 'Email not found';", 3);
+      push('statusCode = 404;', 3);
+    }
     push('}', 2);
     endSection('payload');
     push('} catch (e) {', 1);
@@ -170,7 +109,54 @@ export const aws: Client = {
     push('return {', 1);
     push('statusCode,', 2);
     push("headers: { 'Content-Type': 'application/json' },", 2);
-    push('body: JSON.stringify({ apiKey, message: error }),', 2);
+    push('body: JSON.stringify({', 2);
+
+    if (server.length) {
+      push('// OAS Server variables', 3);
+      server.forEach(data => {
+        pushVariable(
+          `${escapeForObjectKey(data.name)}: '${escapeForSingleQuotes(
+            data.default || data.default === '' ? data.default : data.name
+          )}',`,
+          {
+            type: 'server',
+            name: data.name,
+            indentationLevel: 3,
+          }
+        );
+      });
+      blank();
+    }
+
+    if (security.length) {
+      push('// OAS Security variables', 3);
+      security.forEach(data => {
+        if (data.type === 'http') {
+          // Only HTTP Basic auth has any special handling for supplying auth.
+          if (data.scheme === 'basic') {
+            pushVariable(`${escapeForObjectKey(data.name)}: { user: email, pass: apiKey },`, {
+              type: 'security',
+              name: data.name,
+              indentationLevel: 3,
+            });
+            return;
+          }
+        }
+
+        pushVariable(`${escapeForObjectKey(data.name)}: apiKey,`, {
+          type: 'security',
+          name: data.name,
+          indentationLevel: 3,
+        });
+      });
+    } else {
+      push("// The user's API key", 3);
+      push('apiKey,', 3);
+    }
+    blank();
+    push('// Error message, if any', 3);
+    push('message: error,', 3);
+    push('}),', 2);
     push('};', 1);
     push('};');
 
