27484a642e959c74bcf8a30d12afb169aef94a85
Dom Harrington <domharrington@protonmail.com>
2023-04-05 14:09:05 +0100

    Revert "Revert "chore(ruby): lint""
    This reverts commit 2c27f2a6277da6681aa9727ebde9eb8adf6be234.

--- packages/ruby/spec/readme/har/response_serializer_spec.rb
+++ packages/ruby/spec/readme/har/response_serializer_spec.rb
@@ -31,7 +31,7 @@ RSpec.describe Readme::Har::ResponseSerializer do
       expect(json[:status]).to eq 200
       expect(json[:statusText]).to eq 'OK'
       expect(json[:httpVersion]).to eq request.http_version
-      expect(json[:headers]).to match_array(
+      expect(json[:headers]).to contain_exactly(
         [
           { name: 'X-Custom', value: 'custom' },
           { name: 'reject', value: '[REDACTED 6]' }
@@ -40,7 +40,7 @@ RSpec.describe Readme::Har::ResponseSerializer do
       expect(json[:headersSize]).to eq(-1)
       expect(json[:bodySize]).to eq response.content_length
       expect(json[:redirectURL]).to eq ''
-      expect(json[:cookies]).to match_array(
+      expect(json[:cookies]).to contain_exactly(
         [
           { name: 'cookie1', value: 'value1' },
           { name: 'reject', value: '[REDACTED 6]' }
