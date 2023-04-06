1ca1219d7a64d962ab68727d7c55a7fa9b739d5e
Dom Harrington <domharrington@protonmail.com>
2023-04-06 17:10:14 +0100

    Publish other packages:
    - ruby: readme-metrics@2.3.0
    - python: readme-metrics@2.2.3

--- packages/python/readme_metrics/__init__.py
+++ packages/python/readme_metrics/__init__.py
@@ -1,4 +1,4 @@
 from readme_metrics.MetricsApiConfig import MetricsApiConfig
 from readme_metrics.MetricsMiddleware import MetricsMiddleware
 
-__version__ = "2.2.2"
+__version__ = "2.2.3"
--- packages/ruby/lib/readme/metrics/version.rb
+++ packages/ruby/lib/readme/metrics/version.rb
@@ -2,6 +2,6 @@
 
 module Readme
   class Metrics
-    VERSION = '2.2.0'
+    VERSION = '2.3.0'
   end
 end
