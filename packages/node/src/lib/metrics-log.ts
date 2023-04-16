0d8bc4612eea19c4de95ab95ed3777c218b12876
Ryan Park <ryan@ryanpark.org>
2023-04-16 01:45:42 -0700

    feat: backoff requests to the ReadMe server after certain HTTP responses

--- packages/node/src/lib/metrics-log.ts
+++ packages/node/src/lib/metrics-log.ts
@@ -41,6 +41,37 @@ export interface LogResponse {
   ids: string | string[];
 }
 
+const BACKOFF_SECONDS = 15; // when we need to backoff HTTP requests, pause for seconds
+
+let backoffExpiresAt: Date;
+
+// Exported for use in unit tests
+export function setBackoff(expiresAt: Date | undefined) {
+  backoffExpiresAt = expiresAt;
+}
+
+function shouldBackoff(response: Response) {
+  // Some HTTP error codes indicate a problem with the API key, like the key is
+  // invalid or it's being rate limited. To avoid pointless requests to the
+  // ReadMe server, pause outgoing requests for a few seconds before trying
+  // again. Logs will be silently discarded while requests are paused, which is
+  // acceptable since the server wouldn't accept them anyway.
+  switch (response.status) {
+    case 401: // Unauthorized, i.e. this API key is invalid
+      return true;
+    case 403: // Forbidden, i.e. this API key is blocked by the server
+      return true;
+    case 429: // Too Many Requests, i.e. this API key is currently being rate limited
+      return true;
+    case 500: // Internal Server Error; give the ReadMe server a chance to recover
+      return true;
+    case 503: // Service Unavailable; same thing
+      return true;
+    default:
+      return false;
+  }
+}
+
 function getLogIds(body: OutgoingLogBody | OutgoingLogBody[]): string | string[] {
   if (Array.isArray(body)) {
     return body.map(value => value._id);
@@ -58,6 +89,13 @@ export function metricsAPICall(
   const encodedKey = Buffer.from(`${readmeAPIKey}:`).toString('base64');
 
   const makeRequest = () => {
+    if (backoffExpiresAt) {
+      if (backoffExpiresAt > new Date()) {
+        return Promise.resolve();
+      }
+      // after the backoff expires, erase the old expiration time
+      backoffExpiresAt = undefined;
+    }
     return fetch(new URL('/v1/request', config.host), {
       method: 'post',
       body: JSON.stringify(body),
@@ -67,9 +105,20 @@ export function metricsAPICall(
         'User-Agent': `${pkg.name}/${pkg.version}`,
       },
       signal,
-    }).finally(() => {
-      timeoutSignal.clear(signal);
-    });
+    })
+      .then(response => {
+        if (shouldBackoff(response)) {
+          // backoff for a few seconds, but not if another callback has already started backing off
+          if (!backoffExpiresAt) {
+            backoffExpiresAt = new Date();
+            backoffExpiresAt.setSeconds(backoffExpiresAt.getSeconds() + BACKOFF_SECONDS);
+          }
+        }
+        return response;
+      })
+      .finally(() => {
+        timeoutSignal.clear(signal);
+      });
   };
 
   if (fireAndForget) {
