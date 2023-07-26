8e048c89274f884ef096dd8608abd59d9732224c
Dom Harrington <domharrington@protonmail.com>
2023-07-26 12:13:11 +0100

    chore(python): lint

--- packages/python/readme_metrics/tests/django_test.py
+++ packages/python/readme_metrics/tests/django_test.py
@@ -1,6 +1,6 @@
 from datetime import datetime, timedelta
 import time
-from unittest.mock import Mock, MagicMock
+from unittest.mock import Mock
 
 from readme_metrics import MetricsApiConfig
 from readme_metrics.django import MetricsMiddleware
@@ -13,6 +13,7 @@ mock_config = MetricsApiConfig(
     buffer_length=1000,
 )
 
+
 class TestDjangoMiddleware:
     def test(self):
         response = Mock()
@@ -24,7 +25,7 @@ class TestDjangoMiddleware:
         middleware.metrics_core = Mock()
 
         request = Mock()
-        request.headers = {'Content-Length': '123'}
+        request.headers = {"Content-Length": "123"}
         middleware(request)
         # the middleware should call get_response(request)
         get_response.assert_called_once_with(request)
@@ -52,11 +53,13 @@ class TestDjangoMiddleware:
         assert call_args[0][0] == request
         assert isinstance(call_args[0][1], ResponseInfoWrapper)
         assert call_args[0][1].headers.get("X-Header") == "X Value!"
-        assert getattr(request, 'rm_content_length') == request.headers['Content-Length']
+        assert (
+            getattr(request, "rm_content_length") == request.headers["Content-Length"]
+        )
 
     def test_missing_content_length(self):
         middleware = MetricsMiddleware(Mock(), config=mock_config)
         request = Mock()
         request.headers = {}
         middleware(request)
-        assert getattr(request, 'rm_content_length') == "0"
+        assert getattr(request, "rm_content_length") == "0"
