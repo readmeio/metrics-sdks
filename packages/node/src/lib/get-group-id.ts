2a93cd8bab74cbe1ac683905a46d5813afe1c168
Jon Ursenbach <erunion@users.noreply.github.com>
2024-07-29 14:19:25 -0700

    chore(node): removing our dependency on `oas` (#1054)
    ## 🧰 Changes
    
    ~~I don't totally understand why we're loading `oas` in for types on the
    `getGroupIdByOperation` method that by all accounts seems to be unused,
    but because the version of `oas` we are using is pretty old I'm
    upgrading it.~~
    
    Because `oas` is currently being used solely for
    `getGroupIdByOperation`, a method that is not being used anywhere, I am
    removing `getGroupIdByOperation` and our `oas` dependency.
    
    ---------
    
    Co-authored-by: Kanad Gupta <8854718+kanadgupta@users.noreply.github.com>

--- packages/node/src/lib/get-group-id.ts
+++ packages/node/src/lib/get-group-id.ts
@@ -1,22 +1,6 @@
-import type { ApiKey, GroupingObject, KeyValue } from './ReadMe';
-import type { Operation } from 'oas';
+import type { ApiKey, GroupingObject } from './ReadMe';
 
 import get from 'lodash/get';
-
-/**
- * Parses the basic auth object to return the user
- */
-const parseBasicAuth = (key: KeyValue): string | undefined => {
-  if (typeof key === 'object' && 'user' in key) {
-    return key.user;
-  }
-
-  if (typeof key === 'string') {
-    return key;
-  }
-
-  return undefined;
-};
 
 /**
  * Finds the relevant key and its path for a given apiKey
@@ -58,42 +42,6 @@ const findRelevantKey = (keys: ApiKey[], apiKey: string) => {
 };
 
 /**
- * Builds a flattened array of security requirements for an operation.
- */
-const buildFlattenedSecurityRequirements = (operation: Operation): string[] => {
-  if (!operation) {
-    return [];
-  }
-
-  const securityRequirements = operation.getSecurity();
-  return securityRequirements.reduce((acc: string[], curr) => {
-    return [...acc, ...Object.keys(curr)];
-  }, []);
-};
-
-/**
- * Gets the group ID from the keys array for a given security scheme
- */
-const getGroupFromKeysBySecuritySchemes = (keys: ApiKey[] = [], securitySchemes: string[] = []) => {
-  if (!keys || !keys.length) {
-    return undefined;
-  }
-
-  // Look for thr first key containing a security scheme
-  const relevantKey =
-    keys.find(key => {
-      return [...securitySchemes].some(keyName => key[keyName]);
-    }) ?? keys[0];
-
-  const groupKey = ['id', 'apiKey', ...securitySchemes, 'user', 'label', 'name'].find(k => !!relevantKey[k]);
-  if (!groupKey) {
-    return undefined;
-  }
-
-  return parseBasicAuth(relevantKey[groupKey] as KeyValue);
-};
-
-/**
  * Gets the group ID from the keys array for a given apiKey
  */
 const getGroupFromKeysByApiKey = (keys: ApiKey[] = [], apiKey = '') => {
@@ -113,19 +61,6 @@ const getGroupFromKeysByApiKey = (keys: ApiKey[] = [], apiKey = '') => {
     id: get(relevantKey, groupKey) as string,
     label: get(relevantKey, 'label') || get(relevantKey, 'name'),
   };
-};
-
-/**
- * Gets the group ID from the user object for a given security scheme
- */
-const getFromUser = (user: GroupingObject, securitySchemes: string[]): string | undefined => {
-  const groupKey = ['id', 'apiKey', ...securitySchemes, 'user', 'email'].find(k => !!user[k]);
-
-  if (!groupKey) {
-    return undefined;
-  }
-
-  return parseBasicAuth(user[groupKey] as KeyValue);
 };
 
 /**
@@ -154,52 +89,6 @@ export const getGroupByApiKey = (user: GroupingObject, apiKey: string) => {
   if (fromKeysArray) {
     // groupId found in keys
     return fromKeysArray;
-  }
-
-  return undefined;
-};
-
-/**
- * Gets the group ID from the user object for the current operation.
- * It will first look in the keys array, then in the top level user object.
- *
- * When searching the keys array it will look for a key matching the following priority:
- * 1. The key contains the security scheme name for the operation
- * 2. The first key in the array
- *
- * If a key is found it will return the group ID matching the following priority:
- * 1. The key "id"
- * 2. The key "apiKey"
- * 3. The key matching security scheme value
- * 4. The key "user" (for basic auth)
- * 5. The key "label" (for basic auth)
- * 6. The key "name"
- *
- * If no key is found it will return the group ID from the user object matching the following priority:
- * 1. The user "id"
- * 2. The user "apiKey"
- * 3. The user matching security scheme value
- * 4. The user "user" (for basic auth)
- * 5. The user "email"
- */
-export const getGroupIdByOperation = (user: GroupingObject, operation: Operation) => {
-  if (!user) {
-    return undefined;
-  }
-
-  const flattenedSecurityRequirements = buildFlattenedSecurityRequirements(operation);
-
-  const fromKeysArray = getGroupFromKeysBySecuritySchemes(user.keys, flattenedSecurityRequirements);
-
-  if (fromKeysArray) {
-    // groupId found in keys
-    return fromKeysArray;
-  }
-
-  const fromUser = getFromUser(user, flattenedSecurityRequirements);
-  if (fromUser) {
-    // groupId found in user
-    return fromUser;
   }
 
   return undefined;
