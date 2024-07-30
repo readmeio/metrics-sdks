3e16214894e6f3eb8afc7e2ed2407bb03f850be6
Jon Ursenbach <jon@ursenba.ch>
2024-07-29 18:02:15 -0700

    fix: broken tests

--- test/integration-metrics.test.js
+++ test/integration-metrics.test.js
@@ -512,7 +512,7 @@ describe('Metrics SDK Integration Tests', function () {
 
       expect(request.method).toBe('POST');
       expect(request.headers).to.have.header('content-type', /multipart\/form-data; boundary=(.*)/);
-      expect(request.headers).to.have.header('content-length', 982);
+      expect(request.headers).to.have.header('content-length', [960, 982]);
       expect(request.postData.mimeType).toMatch(/multipart\/form-data; boundary=(.*)/);
 
       const owlbertDataURL = await fs.readFile('./test/__datasets__/owlbert.dataurl.json').then(JSON.parse);
