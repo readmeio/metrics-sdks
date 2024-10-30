7865df4402398384d4f2b1fcec7bf9821dfaef51
Andrii Andreiev <129078694+AndriiAndreiev@users.noreply.github.com>
2024-10-30 13:49:27 +0200

    feat(dotnet): refacroting DI, test api caller

--- packages/dotnet/ReadMe/HarJsonTranslationLogics/ReadmeApiCaller.cs
+++ packages/dotnet/ReadMe/HarJsonTranslationLogics/ReadmeApiCaller.cs
@@ -5,22 +5,23 @@ using RestSharp;
 
 namespace ReadMe.HarJsonTranslationLogics
 {
-  class ReadMeApiCaller
+  public class ReadMeApiCaller
   {
     private readonly string harJsonObject;
     private readonly string apiKey;
+    private readonly IRestClient client;
 
-    public ReadMeApiCaller(string harJsonObject, string apiKey)
+    public ReadMeApiCaller(string harJsonObject, string apiKey, IRestClient client)
     {
       this.harJsonObject = harJsonObject;
       this.apiKey = apiKey;
+      this.client = client;
     }
 
     public void SendHarObjToReadMeApi()
     {
       try
       {
-        var client = new RestClient(ConstValues.ReadmeAPIEndpoints);
         var request = new RestRequest(Method.POST);
         request.AddHeader("Content-Type", "application/json");
         string apiKey = "Basic " + Convert.ToBase64String(Encoding.UTF8.GetBytes(this.apiKey + ":"));
