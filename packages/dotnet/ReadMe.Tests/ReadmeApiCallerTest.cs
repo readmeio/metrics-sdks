5f51d5caac00252c4374e0b3b031b5865ba5e2f6
Andrii Andreiev <129078694+AndriiAndreiev@users.noreply.github.com>
2024-10-30 14:00:10 +0200

    Update ReadmeApiCallerTest.cs

--- packages/dotnet/ReadMe.Tests/ReadmeApiCallerTest.cs
+++ packages/dotnet/ReadMe.Tests/ReadmeApiCallerTest.cs
@@ -5,35 +5,36 @@ using NUnit.Framework;
 using RestSharp;
 using ReadMe.HarJsonTranslationLogics;
 
-namespace ReadMe.Tests;
-
-public class ReadmeApiCallerTest
+namespace ReadMe.Tests
 {
-    private const string testHarJsonObject = "{\"sample\":\"data\"}";
-    private const string testApiKey = "testApiKey";
-    private Mock<IRestClient> restClient;
-    private ReadMeApiCaller apiCaller;
-
-    [SetUp]
-    public void Setup()
+    public class ReadmeApiCallerTest
     {
-        restClient = new Mock<IRestClient>();
-        apiCaller = new ReadMeApiCaller(testHarJsonObject, testApiKey, restClient.Object);
-    }
+        private const string testHarJsonObject = "{\"sample\":\"data\"}";
+        private const string testApiKey = "testApiKey";
+        private Mock<IRestClient> restClient;
+        private ReadMeApiCaller apiCaller;
 
-    [Test]
-    public void SendHarObjToReadMeApi_ShouldSendRequestWithCorrectHeadersAndBody()
-    {
-        var expectedApiKeyHeader = "Basic " + Convert.ToBase64String(Encoding.UTF8.GetBytes(testApiKey + ":"));
-        restClient.Setup(client => client.ExecuteAsync(It.IsAny<IRestRequest>(), It.IsAny<CancellationToken>())).Verifiable();
+        [SetUp]
+        public void Setup()
+        {
+            restClient = new Mock<IRestClient>();
+            apiCaller = new ReadMeApiCaller(testHarJsonObject, testApiKey, restClient.Object);
+        }
 
-        apiCaller.SendHarObjToReadMeApi();
+        [Test]
+        public void SendHarObjToReadMeApi_ShouldSendRequestWithCorrectHeadersAndBody()
+        {
+            var expectedApiKeyHeader = "Basic " + Convert.ToBase64String(Encoding.UTF8.GetBytes(testApiKey + ":"));
+            restClient.Setup(client => client.ExecuteAsync(It.IsAny<IRestRequest>(), It.IsAny<CancellationToken>())).Verifiable();
 
-        restClient.Verify(client => client.ExecuteAsync(It.Is<RestRequest>(request =>
-            request.Method == Method.POST &&
-            request.Parameters.Exists(p => p.Name == "Content-Type" && (string?)p.Value == "application/json") &&
-            request.Parameters.Exists(p => p.Name == "Authorization" && (string?)p.Value == expectedApiKeyHeader) &&
-            request.Parameters.Exists(p => p.Type == ParameterType.RequestBody && (string?)p.Value == testHarJsonObject)
-        ), It.IsAny<CancellationToken>()), Times.Once);
+            apiCaller.SendHarObjToReadMeApi();
+
+            restClient.Verify(client => client.ExecuteAsync(It.Is<RestRequest>(request =>
+                request.Method == Method.POST &&
+                request.Parameters.Exists(p => p.Name == "Content-Type" && (string?)p.Value == "application/json") &&
+                request.Parameters.Exists(p => p.Name == "Authorization" && (string?)p.Value == expectedApiKeyHeader) &&
+                request.Parameters.Exists(p => p.Type == ParameterType.RequestBody && (string?)p.Value == testHarJsonObject)
+            ), It.IsAny<CancellationToken>()), Times.Once);
+        }
     }
 }
